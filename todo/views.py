818755e3ed213448a07947441df26d7092839759
1F10220215 <s1f102202156@iniad.org>
2023-07-23 23:01:17 +0900

    いろいろ

--- todo/views.py
+++ todo/views.py
@@ -8,7 +8,7 @@ from todo.models import Task
 # Create your views here.
 def index(request):
     if request.method == "POST":
-        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])),detail_content=request.POST['detail_content'])
+        task = Task(title=request.POST['title'], due_at=make_aware(parse_datetime(request.POST['due_at'])))
         task.save()
 
     if request.GET.get('order') == 'due':
@@ -37,16 +37,17 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
+
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
-        return redirect(detail, task_id)
+        return redirect('detail', task_id=task_id)
 
-        context = {
-            'task': task
-        }
-        return render(request, "todo/edit.html", context)
+    context = {
+        'task': task
+    }
+    return render(request, "todo/edit.html", context)
 
 def close(request, task_id):
     try:
