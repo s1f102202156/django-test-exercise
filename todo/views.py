635bcaa95b6b5b7aacf3cc77721199683204d96e
1f10220213 <s1f102202134@iniad.org>
2023-07-26 15:50:50 +0900

    一番提出期限が近い奴が赤色になって一番上に表示されるやつ

--- todo/views.py
+++ todo/views.py
@@ -16,8 +16,11 @@ def index(request):
     else:
         tasks = Task.objects.order_by('-posted_at')
 
+    latest = Task.objects.order_by('due_at')[0]
+
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'latest': latest
     }
     return render(request, 'todo/index.html', context)
 
@@ -37,16 +40,17 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
+
     if request.method == 'POST':
         task.title = request.POST['title']
         task.due_at = make_aware(parse_datetime(request.POST['due_at']))
         task.save()
-        return redirect(detail, task_id)
+        return redirect('detail', task_id=task_id)
 
-        context = {
-            'task': task
-        }
-        return render(request, "todo/edit.html", context)
+    context = {
+        'task': task
+    }
+    return render(request, "todo/edit.html", context)
 
 def close(request, task_id):
     try:
